6263cf25171465f300388ba1cbf84218f6f45e1e
Simon Vrachliotis <simon@simonswiss.com>
2025-03-04 12:30:39 +1100

    before-html-port

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,7 @@
-import ReactDOM from 'react-dom/client'
-import './app.css'
-
-const tech = 'React and Vite'
-
-function App() {
-  return <h1 className="font-mono text-4xl">Hello {tech}!</h1>
+export function App() {
+  return (
+    <div>
+      <p>ENTIRE APP</p>
+    </div>
+  );
 }
-
-const app = document.getElementById('app')
-const root = ReactDOM.createRoot(app)
-
-root.render(<App />)
