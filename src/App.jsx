4fc52ed5f158680b0e54b0594e8ac5faa177c28a
Simon Vrachliotis <simon@simonswiss.com>
2025-03-11 10:21:16 +1100

    end of lesson 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import { PageWrapper } from "./components/PageWrapper";
 import { Container } from "./components/Container";
 import { Header } from "./components/Header";
+import { Search } from "./components/Search";
+import { Shortlist } from "./components/Shortlist";
+import { PuppiesList } from "./components/PuppiesList";
+import { NewPuppyForm } from "./components/NewPuppyForm";
 
 export function App() {
   return (
     <PageWrapper>
       <Container>
         <Header />
-        {/* more to come */}
+        <Main />
       </Container>
     </PageWrapper>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <div className="mt-24 grid gap-8 sm:grid-cols-2">
+        <Search />
+        <Shortlist />
+      </div>
+      <PuppiesList />
+      <NewPuppyForm />
+    </main>
   );
 }
